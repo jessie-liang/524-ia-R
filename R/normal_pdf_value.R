77686bd61ff4fd9da67172a675089f0b01a6038b
Jessie Liang <rnliang.jessie@gmail.com>
2026-01-07 11:05:50 -0800

    documentation done

--- R/normal_pdf_value.R
+++ R/normal_pdf_value.R
@@ -1,3 +1,14 @@
+#' Calculate PDF value of any noraml distributions anywhere
+#'
+#' @param mu The mean of the normal distribution
+#' @param sigma The standard deviation of the normal distribution
+#' @param x The location at which you want to evaluate the x-value
+#'
+#' @return A real number
+#' @export
+#'
+#' @examples
+#' normal_pdf_value(0, 1, 0)
 normal_pdf_value <- function(mu, sigma, x) {
 
   constant <- 1 / (sigma * sqrt(2 * pi))
